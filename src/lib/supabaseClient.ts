3c753283f73fdf6b33066e97d42ec3f54b46d6e6
Mayank Sharma <mayanksharma@gmail.com>
2026-05-11 01:09:32 +0530

    chore: secure supabase credentials in env variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ijqfezqknxiyrphceeop.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqcWZlenFrbnhpeXJwaGNlZW9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MzE5ODIsImV4cCI6MjA5NDAwNzk4Mn0.gI449HGAYprmlmKLyZb9hrjDY-cXmejZXc_7SOYGz6s';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
